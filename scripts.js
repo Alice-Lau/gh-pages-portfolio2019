85323744793a1c88413f683f6a4fd455ade12392
Alice-Lau <alice.p.k.lau@gmail.com>
2019-06-19 15:53:15 -0400

    changing position fixed to absolute in attempt to solve sticky nav weird horizontal scroll bug

--- scripts.js
+++ scripts.js
@@ -131,9 +131,9 @@ $overlay.hover(
     }
 );
 
-$overlay.on('click', function() {
-    $title.classList.toggle('visuallyHidden');
-    $usedSkills.classList.toggle('visuallyHidden');
-    $projectDescription.classList.toggle('visuallyHidden');
-    $links.classList.toggle('visuallyHidden');
-})
+// $overlay.on('click', function() {
+//     $title.classList.toggle('visuallyHidden');
+//     $usedSkills.classList.toggle('visuallyHidden');
+//     $projectDescription.classList.toggle('visuallyHidden');
+//     $links.classList.toggle('visuallyHidden');
+// })
